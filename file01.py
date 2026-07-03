ebd1f710b3e6f8ccd3befc29172ab71a03da5e0a
PremGorecki <104720554+PremGorecki@users.noreply.github.com>
2026-07-03 15:13:06 +0200

    lokalny 1 (#5)

--- file01.py
+++ file01.py
@@ -2,7 +2,7 @@
 
 class user():
     def __init__(self, name, age):
-        self.name = name + 'Prem' + Neśka
+        self.name = name + 'Prem' + Yanka + Neśka
         self.age = age
 
     def display_info(self):
